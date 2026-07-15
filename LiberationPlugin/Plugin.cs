7a50781b559a61eae125bc4fa885b40240b4dc89
KarlitoCZ <100475700+KarlitoCZ@users.noreply.github.com>
2026-07-16 00:58:41 +0200

    desc, version number

--- LiberationPlugin/Plugin.cs
+++ LiberationPlugin/Plugin.cs
@@ -12,9 +12,9 @@ public class LiberationPlugin : Plugin<Config>
     public static Plugin Instance { get; set; } = null!;
     public static Config PluginConfig { get; set; } = null!;
     public override string Name { get; } = "Liberation Plugin";
-    public override string Description { get; } = "This plugin does magic!";
+    public override string Description { get; } = "Add The Liberators to your SCP:SL server";
     public override string Author { get; } = "Karlito";
-    public override Version Version { get; } = new (0, 1, 0, 0);
+    public override Version Version { get; } = new (0, 2, 0, 0);
     public override Version RequiredApiVersion { get; } = new (LabApiProperties.CompiledVersion);
     
     private static EventsHandler Events = new EventsHandler();
