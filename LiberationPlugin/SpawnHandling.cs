396e8cac4dc2ea64c79d796dc991e21c93266f92
KarlitoCZ <100475700+KarlitoCZ@users.noreply.github.com>
2026-07-29 22:49:45 +0200

    fixed silly typo

--- LiberationPlugin/SpawnHandling.cs
+++ LiberationPlugin/SpawnHandling.cs
@@ -131,7 +131,7 @@ public sealed class SpawnHandling
         //TimerAPI.UnregisterProperty("custom_role");
         ActiveLiberationPlayers.Remove(player);
         player.Player.CustomInfo = "";
-        player.Player.ReferenceHub.nicknameSync.ShownPlayerInfo &= PlayerInfoArea.Role;
+        player.Player.ReferenceHub.nicknameSync.ShownPlayerInfo |= PlayerInfoArea.Role;
     }
 
     public bool SpawnWave()
